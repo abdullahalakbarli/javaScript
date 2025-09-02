b94dcd9a6a62be2db4d9cdf5333a996aa6f1c796
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-09-02 21:18:54 +0400

    Create dogyears.js

---
+++ learning/projects/dogyears.js
@@ -0,0 +1,17 @@
+// My age in human years
+let myAge = 17;
+// The first two years of a dog's life
+let earlyYears = 2;
+earlyYears *= 10.5;
+// The remaining years after the first 2
+let laterYears = myAge - 2;
+laterYears *= 4;
+// Log the calculated dog years for the first 2 years and the remaining years
+console.log(earlyYears);
+console.log(laterYears);
+// Total age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+// Store name in lowercase
+const myName = 'Abdullah'.toLowerCase();
+// Print a summary statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
