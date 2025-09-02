d7fc258fe6a73e477746db9a0939b620727a40b0
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-09-02 20:58:50 +0400

    Create kelvinweather.js

---
+++ kelvinweather.js
@@ -0,0 +1,7 @@
+//variable named kelvin, and equal to 333 (program work for any Kelvin temperature)
+const kelvin = 333;
+//variable named celsius
+let celsius = kelvin - 273;
+//variable named fahrenheit
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
